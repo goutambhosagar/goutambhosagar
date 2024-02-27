e738974484100ddfe3a74d54135c3cd7ba968dc5
goutambhosagar <157117814+goutambhosagar@users.noreply.github.com>
2024-02-27 20:40:08 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,17 @@
+function showmenu(){
+    var b = document.getElementById("bar");
+    var t = document.getElementById("times");
+    var m = document.getElementById("mob-menu");
+    b.style.display = "none"
+    t.style.display = "block"
+    m.style.display = "block"
+}
+function closemenu(){
+    var b = document.getElementById("bar");
+    var t = document.getElementById("times");
+    var m = document.getElementById("mob-menu");
+    b.style.display = "block"
+    t.style.display = "none"
+    m.style.display = "none"
+}
+
